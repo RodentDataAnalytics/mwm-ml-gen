86d2bba3a3e2945c351d945d92f577185dfd946d
Avgoustinos Vouros <av.vouros@gmail.com>
2016-11-24 13:53:45 +0000

    scripts

--- unused/match_segments_test.m
+++ unused/match_segments_test.m
@@ -1,19 +1,28 @@
+ppath = 'I:\Documents\MWMGEN\tiago_original\segmentation';
 mclassification = 'I:\Documents\MWMGEN\tiago_original\Mclassification\class_2322_29476_250_09_10_10_mr0-correct2\merged_1.mat';
 output_file = 'I:\Documents\labels.csv';
 
+files = dir(fullfile(ppath,'*.mat'));
+segmentations = cell(1,length(files));
+for i = 1:length(files)
+    load(fullfile(ppath,files(i).name));
+    segmentations{i} = segmentation_configs;
+end
+[output,~] = match_segments(segmentations);
+
 load(mclassification);
-labels = classification_configs.CLASSIFICATION.class_map';
-segments = 1:length(labels);
-segments = segments';
-table = [num2cell(segments), num2cell(labels)];
+labels = classification_configs.CLASSIFICATION.class_map(output(:,2))';
+table = [num2cell(output(:,1)), num2cell(labels)];
 tags = classification_configs.CLASSIFICATION_TAGS;
 for i = 1:size(table,1)
     if table{i,2} > 0
         table{i,2} = tags{table{i,2}}{1};
     else
-        table{i,2} = 'UD';
+        table{i,1} = [];
+        table{i,2} = [];
     end
 end
+table(all(cellfun(@isempty,table),2),:) = [];
 
 VariableNames = cell(1,length(tags)+2);
 VariableNames{1} = 'Segment';
